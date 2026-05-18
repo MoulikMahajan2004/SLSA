cf34600e7f86d0c1e08fb55c550c36db57e8de10
Moulik Mahajan <moulikmahajan2004@gmail.com>
2026-05-18 17:20:15 +1000

    bucket name updation for the state

--- terra/backend.tf
+++ terra/backend.tf
@@ -1,7 +1,7 @@
 #Here I have created th e backend configuration for terraform to store the state file in s3 bucket. This will help me to keep the track of the changes in the infrastructure and also it will help me to collaborate with other team members. I have also added the region where I want to store the state file. In this case, I have chosen ap-southeast-2 region.
 terraform {
   backend "s3" {
-    bucket = "secure-cicd-terraform-state-201727487182"
+    bucket = "terraformstate.01"
     key    = "secure-cicd/terraform.tfstate"
     region = "ap-southeast-2"
   }
