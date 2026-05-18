047d9f19ad2e344576afd9cb7ac3edfc24ba6cbf
Moulik Mahajan <moulikmahajan2004@gmail.com>
2026-05-18 16:12:25 +1000

    updating the ec2 architecture

--- terra/ec2.tf
+++ terra/ec2.tf
@@ -1,13 +1,25 @@
-
 resource "aws_instance" "tfinstance" {
-  instance_type = "t3.micro"
-  ami = "ami-0b8d527345fdace59"
-  //subnet_id = aws_subnet.publicip1
-  subnet_id = aws_subnet.publicip1.id
-  //vpc_security_group_ids = 
-  tags ={
-    name="tfinstances"
+  instance_type               = "t3.micro"
+  ami                         = "ami-0b8d527345fdace59"
+  subnet_id                   = aws_subnet.publicip1.id
+  vpc_security_group_ids      = [aws_security_group.tfsg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "tfinstance"
   }
 }
+
+
+# resource "aws_instance" "tfinstance" {
+#   instance_type = "t3.micro"
+#   ami = "ami-0b8d527345fdace59"
+#   //subnet_id = aws_subnet.publicip1
+#   subnet_id = aws_subnet.publicip1.id
+#   //vpc_security_group_ids = 
+#   tags ={
+#     name="tfinstances"
+#   }
+# }
 
 
--- terra/sg.tf
+++ terra/sg.tf
@@ -1,9 +1,47 @@
 resource "aws_security_group" "tfsg" {
+  name   = "secure-cicd-sg"
   vpc_id = aws_vpc.Tfpipelinevpc.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # for testing only
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
-    Name = "newsgupdatedone"
+    Name = "secure-cicd-sg"
   }
 }
+
+
+#---------------------
+# resource "aws_security_group" "tfsg" {
+#   vpc_id = aws_vpc.Tfpipelinevpc.id
+#   tags = {
+#     Name = "newsgupdatedone"
+#   }
+# }
+
+#--------------------
+
 
 # resource "aws_vpc_security_group_ingress_rule" "name" {
 #   security_group_id = aws_security_group.tfsg.id
