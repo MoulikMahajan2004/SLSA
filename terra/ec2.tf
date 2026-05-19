ec49787e0d1d9e89f1d869df83f29b337a8502fe
Moulik Mahajan <moulikmahajan2004@gmail.com>
2026-05-19 12:42:34 +1000

    test 4

--- terra/ec2.tf
+++ terra/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "tfinstance" {
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   ami                         = "ami-0b8d527345fdace59"
   subnet_id                   = aws_subnet.publicip1.id
   vpc_security_group_ids      = [aws_security_group.tfsg.id]
